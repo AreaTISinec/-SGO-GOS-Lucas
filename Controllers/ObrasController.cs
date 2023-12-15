44fcacb5b8ae0af564d07e16b9108e80667b385d
AnyMal <ldroguett@utem.cl>
2023-12-15 11:59:49 -0300

    primer commit

---
+++ Controllers/ObrasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using test.Datos;
+using test.Model;
+
+namespace test.Controllers
+{
+    [ApiController]
+    [Route("api/obras")]
+    public class ObrasController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<MObras>>> Get()
+        {
+            var funcion = new DObras();
+            var lista = await funcion.MostrarObras();
+            return lista;
+        }
+        [HttpPost]
+        public async Task Post([FromForm] MObras parametros)
+        {
+            var funcion = new DObras();
+            await funcion.InsertarObra(parametros);
+        }
+    }
+}
---
+++ Model/MObras.cs
@@ -0,0 +1,14 @@
+﻿namespace test.Model
+{
+    public class MObras
+    {
+        public int id { get; set; }
+        public decimal monto_neto { get; set; }
+        public int ce_ne { get; set; }
+        public string empresa { get; set; }
+        public string nombre_obra { get; set; }
+        public string responsable { get; set; }
+        public string cliente { get; set; }
+        public string area { get; set; }
+    }
+}
